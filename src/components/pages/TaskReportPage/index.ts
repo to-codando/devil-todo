599c799ac1e7e16c06237a552309f77186c68d6a
Rodrigo ROcha <rocha.webdev@gmail.com>
2024-09-27 18:26:57 -0300

    feat: initial commit

---
+++ src/components/pages/TaskReportPage/index.ts
@@ -0,0 +1,9 @@
+import { tsx } from "iares";
+
+const template = () => tsx`
+  <h1>Report page</h1>
+`;
+
+export const TaskReportPage = () => ({
+  template,
+});
---
+++ src/components/pages/index.ts
@@ -0,0 +1,3 @@
+export { TasksPage } from "./TasksPage";
+export { TaskReportPage } from "./TaskReportPage";
+export { DefaultPage } from "./DefaultPage";
