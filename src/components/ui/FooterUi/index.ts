1912ac3d27d1defd9972d6eb374e94584cb53e41
Rodrigo ROcha <rocha.webdev@gmail.com>
2024-09-28 16:20:16 -0300

    feat: update footer styles

--- src/components/ui/FooterUi/index.ts
+++ src/components/ui/FooterUi/index.ts
@@ -51,17 +51,24 @@ const styles = () => css`
     align-items: center;
     width: 100%;
   }
+  footer-ui {
+    background: #0F0F14
+  }
   .list-ctx {
-    justify-content: space-between;
+    justify-content: space-evenly;
   }
   .list-item-ctx {
-    width: auto
+    justify-content: center;
+    text-align:center;
+    width: auto;
   }
   .list-item-ctx:nth-of-type(3) {
     padding:0.5em;
     background:#128DFF;
     border-radius: 100%;
-    border: 2px #1b1f2b solid;
+    border: 2px var(--page-bg) solid;
+    position: relative;
+    top: -32px
   }
   .list-item-ctx:hover {
     cursor: pointer;
