14809e705cc556c0ed8b55e9b5aa210b4fffac29
Rodrigo ROcha <rocha.webdev@gmail.com>
2024-09-28 17:09:41 -0300

    feat: styling header position

--- src/components/ui/HeaderUi/index.ts
+++ src/components/ui/HeaderUi/index.ts
@@ -1,23 +1,43 @@
 import { css, tsx } from "iares";
 
+type Props = {
+  height?: "50px" | "75px" | "100px" | "125px" | "auto";
+};
+
+type Params = {
+  props: Props;
+};
+
 const template = () => tsx`
   <h1 class="title-ctx">Header ui</h1>
 `;
 
-export const HeaderUi = () => ({
-  template,
-  styles,
-});
+export const HeaderUi = ({ props }: Params) => {
+  return {
+    template,
+    styles,
+    props,
+  };
+};
 
-const styles = () => css`
+const styles = ({ props }: Params) => {
+  return css`
   header-ui,
   header-ui > h1 {
     display:flex;
     flex-wrap: wrap;
     align-items: flex-start;
     justify-content: flex-start;
+    width:100%;
+  }
+  header-ui {
+    height: ${props?.height || "75px"};
+    padding:0 1em;
+    position: fixed;
+    top:0;
   }
   .title-ctx {
     color: #ff6600
   }
 `;
+};
